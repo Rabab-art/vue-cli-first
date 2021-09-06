b30e2f6e55506caec2b28e750dbb14753d45c9b1
Rabab-art <bouhamla.rabab@gmail.com>
2021-09-06 17:13:34 +0200

    First  app with Vue

---
+++ src/components/Header.vue
@@ -0,0 +1,14 @@
+<template>
+  <header><h1>La mia prima app con Vue CLI!</h1></header>
+</template>
+<script>
+export default {
+  name: "Header",
+};
+</script>
+
+<style>
+h1 {
+  color: violet;
+}
+</style>
